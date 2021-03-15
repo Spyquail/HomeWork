74cc5e7f6672598537478abd31fc740636251953
Чесноков Сергей <chesnokov.sv@elt.cor>
2021-03-15 16:24:56 +0700

    Добавьте файлы проекта.

---
+++ HomeWork1.2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HomeWork1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 10 символов, обозначающих 4 стороны света на русском языке. Пример: сСюЮвВзЗЗЗ");
+            
+            
+            int x = 0, y = 0;
+            for(int i = 0; i < 10; i++)
+            {
+                char symbol = Convert.ToChar(Console.ReadLine());
+                switch(symbol)
+                {
+                    case 'с': y++; break;
+                    case 'С': y++; break;
+                    case 'ю': y--; break;
+                    case 'Ю': y--; break;
+                    case 'в': x++; break;
+                    case 'В': x++; break;
+                    case 'з': x--; break;
+                    case 'З': x--; break;
+                    default:  Console.WriteLine("Нет такого пути"); break;
+                }
+            }
+            double distace = Math.Sqrt((x * x) + (y * y));
+            Console.WriteLine($"Расстояние до точки: {distace}");
+            
+        }
+    }
+}
---
+++ HomeWork1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HomeWork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите номер месяца");
+            string monthNum = Console.ReadLine();
+            switch (monthNum)
+            {
+                case "1": Console.WriteLine("Это январь!"); break;
+                case "2": Console.WriteLine("Это февраль!"); break;
+                case "3": Console.WriteLine("Это март!"); break;
+                case "4": Console.WriteLine("Это апрель!"); break;
+                case "5": Console.WriteLine("Это май!"); break;
+                case "6": Console.WriteLine("Это июнь!"); break;
+                case "7": Console.WriteLine("Это июль!"); break;
+                case "8": Console.WriteLine("Это август!"); break;
+                case "9": Console.WriteLine("Это сентябрь!"); break;
+                case "10": Console.WriteLine("Это октябрь!"); break;
+                case "11": Console.WriteLine("Это ноябрь!"); break;
+                case "12": Console.WriteLine("Это декабрь!"); break;
+                default: Console.WriteLine("Нет такого месяца!"); break;
+            }
+        }
+    }
+}
