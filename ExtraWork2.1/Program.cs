a19b4269224253f49de2b4e5a4cee9f69a424c00
Чесноков Сергей <spyquail5@gmail.com>
2021-03-25 11:38:51 +0700

    1

--- ExtraWork2.1/Program.cs
+++ ExtraWork2.1/Program.cs
@@ -7,16 +7,80 @@ namespace ExtraWork2._1
         static void Main(string[] args)
         {
             Console.WriteLine("Введите размер матрицы n для формулы (2*n + 1): ");
-            int arraySize = Convert.ToInt32(Console.ReadLine());
-            arraySize = 2 * arraySize + 1;
+            int enterVolue = Convert.ToInt32(Console.ReadLine());
+            int arraySize = 2 * enterVolue + 1;
             int[,] massive = new int[arraySize, arraySize];
-            for (int i = 0; i < massive.GetLength(1); i++) // Цикл для ввода значений
+            int loopVariable = 1; // Переменная для смены направления
+            int count = 0; // Счётчик для увеличения количества шагов
+            int numberValues = 1; // Количество шагов в определённом направлении
+            int positionVertical = enterVolue;  // Координаты элемента массива, с которым работаем в данный момент
+            int positionHorisontal = enterVolue ;
+            int volue = 0; // Переменная для заполнения массива
+            int intermediativeVar = 0; // Промежуточная переменная для передачи значения из цикла
+            while (true) // Цикл для заполнения массива с разными направлениями
+            {
+                switch (loopVariable)
+                {
+                    case 1: // up
+                        for(int y = positionVertical; y >= (positionVertical - numberValues); y--)
+                        {
+                            massive[y, positionHorisontal] = volue; 
+                            volue++;
+                            intermediativeVar = y;
+                            if (volue >= arraySize * arraySize) break; //Условие окончания массива и выход из цикла
+                        }
+                        positionVertical = intermediativeVar;
+                        volue--;
+                        break;
+                    case 2: // left
+                        for (int x = positionHorisontal; x >= (positionHorisontal - numberValues); x--)
+                        {
+                            massive[positionVertical, x] = volue;    
+                            volue++;
+                            intermediativeVar = x;
+                        }
+                        positionHorisontal = intermediativeVar;
+
+                        volue--;
+                        break;
+                    case 3: // down
+                        for (int y = positionVertical; y <= (positionVertical + numberValues); y++)
+                        {
+                            massive[y, positionHorisontal] = volue;
+                            volue++;
+                            intermediativeVar = y;
+                        }
+                        positionVertical = intermediativeVar;
+                        volue--;
+                        break;
+                    case 4: // right
+                        for (int x = positionHorisontal; x <= (positionHorisontal + numberValues); x++)
+                        {
+                            massive[positionVertical, x] = volue;
+                            volue++;
+                            intermediativeVar = x;
+                        }
+                        positionHorisontal = intermediativeVar;
+                        volue--;
+                        break;
+                }
+                count++;
+                if(count == 2) // Условие для увеличения количества шагов и обнуления счётчика
+                {
+                    numberValues++;
+                    count = 0;
+                }
+                if (loopVariable == 4) loopVariable = 1; // Условие для смены направления 
+                else loopVariable++;
+                if (volue >= (arraySize * arraySize) - 2) break; // Условие для выхода из цикла при окончании 
+            }
+            for (int i = 0; i < massive.GetLength(0); i++) // Цикл для вывода массива на консоль
             {
                 for (int j = 0; j < massive.GetLength(1); j++)
                 {
-                    Console.Write($"{i + 1},{j + 1} значение массива: ");
-                    massive[i, j] = EnterValueInt();
+                    Console.Write($"{massive[i, j]} ");
                 }
+                Console.WriteLine();
             }
         }
     }
